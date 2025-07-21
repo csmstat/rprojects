66a080e3922de25ad3cf02842108fb26022a9518
csmstat <43538580+csmstat@users.noreply.github.com>
2025-07-21 18:12:51 +0200

    Delete functions directory

--- functions/aggrega_disposition_time.R
+++
@@ -1,9 +0,0 @@
-aggrega_disposition_time <- function(dt, keyby_cols) {
-  tmp <- dt[, lapply(.SD, function(x) sum(as.numeric(x), na.rm = T)), keyby = keyby_cols, .SDcols = c("Definiti", "Pendenti Finali")]
-  tmp[grep("-06-30", Data), `Disposition Time` := ifelse(`Definiti` > 0, round((`Pendenti Finali`/`Definiti`)*182.5, 0), NA)]  # dati semestrali
-  tmp[grep("-12-31", Data), `Disposition Time` := ifelse(`Definiti` > 0, round((`Pendenti Finali`/`Definiti`)*365,   0), NA)]  # dati annuali
-  tmp[grep("tg26",   Data), `Disposition Time` := ifelse(`Definiti` > 0, round((`Pendenti Finali`/`Definiti`)*365,   0), NA)]  # target
-  tmp <- melt(tmp, id.vars = keyby_cols, measure.vars = c("Disposition Time", "Pendenti Finali"))  # i definiti vengono generati dal successivo aggrega_definiti (per via dei dati semestrali)
-  #tmp <- melt(tmp, id.vars = keyby_cols, measure.vars = c("Disposition Time"))
-  return(tmp)
-  }
--- functions/wrap_by.R
+++
@@ -1,7 +0,0 @@
-wrap_by <- function(..., is_grid = F) {
-  if(is_grid) {
-    facet_grid(vars(...), scales = "free", labeller = label_value)
-  } else {
-    facet_wrap(vars(...), scales = "free", labeller = label_value)
-  }
-}
